ac5349670849c62df252a44fe990debf46b4b8e8
Mauro <maurodefalco96@gmail.com>
2025-01-12 20:10:49 +0100

    dark mode based on settings

--- quasar.config.js
+++ quasar.config.js
@@ -73,7 +73,7 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
       config: {
-        dark: true
+        dark: 'auto'
       },
 
       // iconSet: 'material-icons', // Quasar icon set
