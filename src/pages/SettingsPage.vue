e1122341b7c37e6b93eff6a9a8e3264c4a34c427
Mauro <maurodefalco96@gmail.com>
2025-01-15 20:10:08 +0100

    delete form

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -2,11 +2,6 @@
   <q-page class="flex flex-center">
     <h1>Settings Page</h1>
 
-    <div>
-  <q-btn label="Submit" type="submit" color="primary"/>
-  <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-</div>
-
   </q-page>
 </template>
 
