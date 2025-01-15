c318c9cf9bf40fab7e1bca361228951366d5cd5b
Mauro <maurodefalco96@gmail.com>
2025-01-15 20:09:56 +0100

    form component

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,6 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <h1>Settings Page</h1>
+
+    <div>
+  <q-btn label="Submit" type="submit" color="primary"/>
+  <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+</div>
+
   </q-page>
 </template>
 
