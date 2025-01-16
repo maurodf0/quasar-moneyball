d3042373bba60ccd9d8b2eb7257edd8f46d368cd
Mauro <maurodefalco96@gmail.com>
2025-01-16 23:54:25 +0100

    q-item

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,26 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Entries Page</h1>
+  <q-page>
+      <div class="q-pa-md">
+    <q-list bordered separator>
+      <q-item>
+        <q-item-section>
+          Salary
+        </q-item-section>
+
+          <q-item-section side>
+          + 4.999,99€
+        </q-item-section>
+      </q-item>
+     
+
+    
+    </q-list>
+  </div>
   </q-page>
 </template>
 
 <script setup>
 //
+import { ref } from 'vue'
+
 </script>
