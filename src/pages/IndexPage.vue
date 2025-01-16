aa42ca947f0e677d4ee2b3f7aae5834b64b13cca
Mauro <maurodefalco96@gmail.com>
2025-01-16 23:56:49 +0100

    v-for for entries

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,13 +2,13 @@
   <q-page>
       <div class="q-pa-md">
     <q-list bordered separator>
-      <q-item>
+      <q-item v-for="entry in entries" :key="entry.id">
         <q-item-section>
-          Salary
+          {{ entry.name }}
         </q-item-section>
 
           <q-item-section side>
-          + 4.999,99€
+          {{ entry.amount }}
         </q-item-section>
       </q-item>
      
@@ -23,4 +23,27 @@
 //
 import { ref } from 'vue'
 
+
+const entries = ref([
+  {
+    id: 'id1',
+    name: 'Salary',
+    amount: 4.999,
+  },
+  {
+    id: 'id2',
+    name: 'Rent',
+    amount:-999,
+  },
+  {
+    id: 'id3',
+    name: 'Phone',
+    amount: -19.99,
+  },
+  {
+    id: 'id4',
+    name: 'Unknown',
+    amount: 0,
+  },
+])
 </script>
