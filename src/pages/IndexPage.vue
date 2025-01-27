8b78bbdfede6c1cf6ab628315bf3e10f3ac12544
Mauro <maurodefalco96@gmail.com>
2025-01-27 23:56:27 +0100

    clean up

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,16 +15,11 @@
 <script setup>
 //
 
-import { useQuasar } from 'quasar'
 import { useStoreEntries } from 'src/stores/storeEntries';
-import useCurrency from '../composables/useCurrency';
-import useAmountColorClass from 'src/composables/useAmountColorClass';
 import Balance from 'src/components/Entries/Balance.vue';
 import AddEntry from 'src/components/Entries/AddEntry.vue';
 import Entry from 'src/components/Entries/Entry.vue';
 
-const { currencify } = useCurrency();
 const  storeEntries = useStoreEntries();
-const $q = useQuasar();
 
 </script>
