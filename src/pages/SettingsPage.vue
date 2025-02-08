2c18838538adef6a1f965d518e7122f8ed793d77
Mauro <maurodefalco96@gmail.com>
2025-02-08 19:35:06 +0100

    settings page

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -1,12 +1,24 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Settings Page</h1>
-    <!-- <pre>
-      {{ $route }}
-    </pre> -->
+  <q-page>
+      <div class="q-pa-md" >
+    <q-list bordered padding>
+      <q-item-label header>Entries</q-item-label>
+
+      <q-item tag="label" v-ripple>
+        <q-item-section>
+          <q-item-label>Battery too low</q-item-label>
+        </q-item-section>
+        <q-item-section side >
+          <q-toggle color="blue" v-model="notif1" val="battery" />
+        </q-item-section>
+      </q-item>
+
+     
+    </q-list>
+  </div>
   </q-page>
 </template>
 
 <script setup>
-//
+
 </script>
