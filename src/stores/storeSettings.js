dff07b8deb02e291721a2b655e978c156e01122b
Mauro <maurodefalco96@gmail.com>
2025-02-08 19:30:35 +0100

    setting Store init

---
+++ src/stores/storeSettings.js
@@ -0,0 +1,16 @@
+export const useStoreSettings = defineStore("settings", () => {
+
+const settings = reactive({
+    prompttoDelete: true,
+});
+
+/* HELPERS */
+
+
+
+  return {
+    settings,
+
+  };
+
+});
