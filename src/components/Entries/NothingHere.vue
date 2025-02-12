591577bd48aa14b68eb2d57f7ef622778b873565
Mauro <maurodefalco96@gmail.com>
2025-02-12 23:55:02 +0100

    aggiorna il componente NothingHere per utilizzare colori dinamici in base al tema

--- src/components/Entries/NothingHere.vue
+++ src/components/Entries/NothingHere.vue
@@ -1,8 +1,14 @@
 <template>
     <div class="text-center q-pt-xl">
-        <q-icon name="savings" size="100px" color="grey-4" />
-        <div class="text-grey-5">
+        <q-icon name="savings" size="100px" :color="useLightOrDark('grey-4', 'grey-9')" />
+        <div :class="useLightOrDark('text-grey-4', 'text-grey-7')">
             No entries yet
         </div>
     </div>
 </template>
+
+<script setup>
+
+import useLightOrDark from 'src/composables/useLightOrDark';
+
+</script>
