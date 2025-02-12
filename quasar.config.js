57363bc323a7b5092d8e18b1ab4c3f95023b362e
Mauro <maurodefalco96@gmail.com>
2025-02-12 23:57:51 +0100

    LocalStorage Plugin

--- quasar.config.js
+++ quasar.config.js
@@ -87,7 +87,7 @@ export default defineConfig((/* ctx */) => {
       // directives: [],
 
       // Quasar plugins
-      plugins: ['Dialog', 'Notify']
+      plugins: ['Dialog', 'Notify', 'LocalStorage']
     },
 
     // animations: 'all', // --- includes all animations
