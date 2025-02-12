df5d18a7f41e6f49970fc423a7b98a2720de048a
Mauro <maurodefalco96@gmail.com>
2025-02-12 23:49:41 +0100

    :bg-color="useLightOrDark('white', 'black')

--- src/components/Entries/AddEntry.vue
+++ src/components/Entries/AddEntry.vue
@@ -8,14 +8,15 @@
             </transition>
         <q-form @submit="storeEntries.addEntry(addEntryForm)" class="row q-pa-sm q-gutter-sm bg-primary">
             <div class="col">
-                <q-input  v-select-all
-                ref="storeEntries.nameRef" v-model="addEntryForm.name" placeholder="Name" bg-color="white"
+                <q-input  
+                v-select-all
+                ref="storeEntries.nameRef" v-model="addEntryForm.name" placeholder="Name" :bg-color="useLightOrDark('white', 'black')"
                     outlined dense />
             </div>
             <div class="col">
                 <q-input  v-select-all
                 v-model.number="addEntryForm.amount" type="number" step="0.01" class="text-right"
-                    placeholder="Amount" bg-color="white" outlined dense />
+                    placeholder="Amount" :bg-color="useLightOrDark('white', 'black')" outlined dense />
             </div>
             <div class="col col-auto">
                 <q-btn type="submit" round color="primary" icon="add" />
@@ -31,6 +32,7 @@ import { useStoreEntries } from 'src/stores/storeEntries';
 import Balance from 'src/components/Entries/Balance.vue';
 import vSelectAll from 'src/directives/directiveSelectAll';
 const storeEntries = useStoreEntries();
+import useLightOrDark from 'src/composables/useLightOrDark';
 
 
 const addEntryForm = reactive({
